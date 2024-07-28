bde52b87cd34baf408db45dbcec97241f6da057a
Bukouzan@TD <13790686+battd13@users.noreply.github.com>
2024-07-29 05:32:28 +0900

    Create visitor.js

---
+++ site/scripts/visitor.js
@@ -0,0 +1,11 @@
+function text(url) {
+  return fetch(url).then(res => res.text());
+}
+
+text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
+  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
+  let ip = data.match(ipRegex)[0];
+  let kexRegex = /kex=(.*);
+  let kex = data.match(kexRegex)[1];
+  console.log(ip, kex);
+});
